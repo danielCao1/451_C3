7bc5279313ec8d50de727506f2820ddc5f8a527c
jason <jasonisazn@tamu.edu>
2023-11-28 16:55:37 -0600

    init spring boot app & rename frontend -> app

---
+++ src/main/java/com/c1/enhancedghidra/EnhancedGhidraApplication.java
@@ -0,0 +1,13 @@
+package com.c1.enhancedghidra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnhancedGhidraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnhancedGhidraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/c1/enhancedghidra/EnhancedGhidraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c1.enhancedghidra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnhancedGhidraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

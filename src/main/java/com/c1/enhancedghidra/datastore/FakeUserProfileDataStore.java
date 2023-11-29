5c723f39ce8b889b7d9f061211fe0e6fdcce259e
jason <jasonisazn@tamu.edu>
2023-11-29 04:26:06 -0600

    hard code uid values

--- src/main/java/com/c1/enhancedghidra/datastore/FakeUserProfileDataStore.java
+++ src/main/java/com/c1/enhancedghidra/datastore/FakeUserProfileDataStore.java
@@ -13,9 +13,9 @@ public class FakeUserProfileDataStore {
     private static final List<UserProfile> USER_PROFILES = new ArrayList<>();
 
     static {
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "Jason", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "Daniel", null));
-        USER_PROFILES.add(new UserProfile(UUID.randomUUID(), "Jacob", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("2071f922-6895-420b-be19-cfa3dab7b9b2"), "Jason", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("53e8524b-df45-4fdf-a4ec-9c5f7f5912ff"), "Daniel", null));
+        USER_PROFILES.add(new UserProfile(UUID.fromString("2d06fe31-64ca-48c7-8ebd-478152b7e3fa"), "Jacob", null));
     }
 
     public List<UserProfile> getUserProfiles() {
